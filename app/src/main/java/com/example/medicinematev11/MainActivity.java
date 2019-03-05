df1751abd740f07246b5d49ca9baafa6299b231a
Emil Virolainen <virolainen.emil@gmail.com>
2019-03-05 14:46:15 +0200

    kahen ekan näkymän UI:t ja niiden välillä vaihtelu, ei vielä toimivuutta.

--- app/src/main/java/com/example/medicinematev11/MainActivity.java
+++ app/src/main/java/com/example/medicinematev11/MainActivity.java
@@ -5,11 +5,20 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ListView medicineList;
+    private TextView medicineName;
+    private TextView medicineAmount;
+    private TextView medicineTime;
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,12 +28,45 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+
+                    medicineList.setEnabled(true);                  //sets the list so that it can be used
+                    medicineList.setVisibility(View.VISIBLE);       //sets the list visible
+
+                    medicineName.setEnabled(false);
+                    medicineName.setVisibility(View.INVISIBLE);
+                    medicineAmount.setEnabled(false);
+                    medicineAmount.setVisibility(View.INVISIBLE);
+                    medicineTime.setEnabled(false);
+                    medicineTime.setVisibility(View.INVISIBLE);
+
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+
+                    medicineList.setEnabled(false);
+                    medicineList.setVisibility(View.INVISIBLE);
+
+                    medicineName.setEnabled(true);
+                    medicineName.setVisibility(View.VISIBLE);
+                    medicineAmount.setEnabled(true);
+                    medicineAmount.setVisibility(View.VISIBLE);
+                    medicineTime.setEnabled(true);
+                    medicineTime.setVisibility(View.VISIBLE);
+
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+
+                    medicineList.setEnabled(false);
+                    medicineList.setVisibility(View.INVISIBLE);
+
+                    medicineName.setEnabled(false);
+                    medicineName.setVisibility(View.INVISIBLE);
+                    medicineAmount.setEnabled(false);
+                    medicineAmount.setVisibility(View.INVISIBLE);
+                    medicineTime.setEnabled(false);
+                    medicineTime.setVisibility(View.INVISIBLE);
+
                     return true;
             }
             return false;
@@ -35,6 +77,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        medicineList = (ListView) findViewById(R.id.medicineListView);
+
+        medicineName = (TextView) findViewById(R.id.medicineNameView);
+        medicineAmount = (TextView) findViewById(R.id.medicineAmountView);
+        medicineTime = (TextView) findViewById(R.id.medicineTimeView);
+        medicineName.setEnabled(false);
+        medicineName.setVisibility(View.INVISIBLE);
+        medicineAmount.setEnabled(false);
+        medicineAmount.setVisibility(View.INVISIBLE);
+        medicineTime.setEnabled(false);
+        medicineTime.setVisibility(View.INVISIBLE);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
